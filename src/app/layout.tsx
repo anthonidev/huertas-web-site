cbb1de634d5c5c343afd2df559d6bcecb4c9cf5b
anthonidev <softwaretoni21@gmail.com>
2024-08-07 08:31:37 -0500

    add google adds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import WhatsappButton from '@/components/shared/WhatsappButton';
 import { ProviderToast } from '@/providers/ProviderToast';
 import HeadSocial from '@/components/shared/HeadSocial';
 import { ProviderUI } from '@/providers/ProviderUI';
+import Head from 'next/head';
 
 const inter = Montserrat({ subsets: ['latin'] });
 
@@ -22,7 +23,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang='es'>
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MH9WCJSW');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH9WCJSW"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <ProviderUI>
           <HeadSocial />
           <Navbar />
