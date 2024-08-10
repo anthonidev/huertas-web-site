cf9970b95d6d37efac9431e629104155f40c5873
anthonidev <softwaretoni21@gmail.com>
2024-08-10 18:13:45 -0500

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import WhatsappButton from '@/components/shared/WhatsappButton';
 import { ProviderToast } from '@/components/providers/ProviderToast';
 import { Footer, Navbar } from '@/components/common';
 import Head from 'next/head';
+import { GoogleTagManager } from '@next/third-parties/google';
 const inter = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,26 +19,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <Head>
-        {process.env.NEXT_PUBLIC_HUERTAS_ENV === 'production' && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MH9WCJSW');`,
-            }}
-          />
-        )}
-      </Head>
+      <GoogleTagManager gtmId="GTM-MH9WCJSW" />
       <body className={inter.className}>
-        {process.env.NEXT_PUBLIC_SOKSO_ENV === 'production' && (
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-MH9WCJSW"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-        )}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MH9WCJSW"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ProviderUI>
           <HeadSocial />
           <Navbar />
