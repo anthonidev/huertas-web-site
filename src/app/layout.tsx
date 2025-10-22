cc5c3ebd49e63d27df5b0302a35bb663341a259e
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-10-21 22:58:48 -0500

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,17 @@ export const metadata: Metadata = {
   title: 'Huertas Inmobiliaria',
   description:
     'Obtén TU LOTE AL TOKE con nuestro crédito directo y financiamiento personalizado. Garantiza hoy la seguridad y bienestar de tu familia. ¡Conversemos!',
+  manifest: '/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
